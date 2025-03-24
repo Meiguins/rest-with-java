2d3d01da73c045e4559745ca7df8df7b92d8a05f
Meiguins <kauanmeiguins3@gmail.com>
2025-03-24 18:09:27 -0300

    create project and +

---
+++ src/main/java/br/com/meiguiins/rest_with_java/model/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.meiguiins.rest_with_java.model;
+
+public record Greeting(long id, String content) {}
---
+++ src/test/java/br/com/meiguiins/rest_with_java/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.meiguiins.rest_with_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

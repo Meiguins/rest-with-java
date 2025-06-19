34edac11b912e663dc06106da567ad7339a5cf46
meiguins <kauanmeiguins3@gmail.com>
2025-06-19 02:02:18 -0300

    add person

--- src/main/java/br/com/meiguiins/rest_with_java/model/Person.java
+++ src/main/java/br/com/meiguiins/rest_with_java/model/Person.java
@@ -1,5 +1,15 @@
 package br.com.meiguiins.rest_with_java.model;
 
-public class Person {
+import java.io.Serializable;
 
+public class Person implements Serializable {
+    private static  final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String adrress;
+    private String gender;
+
+    
 }

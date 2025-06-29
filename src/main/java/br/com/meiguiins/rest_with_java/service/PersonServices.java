0e289ee53c6dd5f77baedf1c203f1b957607f218
meiguins <kauanmeiguins3@gmail.com>
2025-06-29 03:19:38 -0300

    Logger test

--- src/main/java/br/com/meiguiins/rest_with_java/service/PersonServices.java
+++ src/main/java/br/com/meiguiins/rest_with_java/service/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.meiguiins.rest_with_java.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import br.com.meiguiins.rest_with_java.exception.ResourceNotFoundException;
 import br.com.meiguiins.rest_with_java.model.Person;
 import br.com.meiguiins.rest_with_java.repository.PersonRepository;
@@ -7,14 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
+
 
 
 @Service
 public class PersonServices  {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger =  LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
